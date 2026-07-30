013bc710601a95f7483c0f2ee84f4f3d2020fd59
akito0802 <light.of.king.a@gmail.com>
2026-07-30 23:47:08 +0900

    モード選択を消して通常ホームを表示

--- splash.js
+++ splash.js
@@ -1,9 +1,6 @@
 (() => {
-  'use strict';
   const params = new URLSearchParams(location.search);
   if (!params.has('song') && params.get('mode') !== 'note') {
     history.replaceState(null, '', `${location.pathname}?mode=note`);
   }
-  document.getElementById('neetLaunch')?.remove();
-  document.body.style.overflow = '';
 })();
