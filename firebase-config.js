672bf3e95dd92ed2d64c64ef998c079497c53d42
akito0802 <light.of.king.a@gmail.com>
2026-08-03 04:54:13 +0900

    クラウド同期設定ファイルを追加

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase ConsoleでWebアプリを追加し、表示された設定値に置き換えてください。
+// このファイルに秘密鍵は含まれませんが、Firestoreのセキュリティルール設定は必須です。
+window.NEET_FIREBASE_CONFIG = window.NEET_FIREBASE_CONFIG || {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
