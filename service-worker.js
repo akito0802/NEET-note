fe4114f887176e7ffb2b08c0c352345708c65ed7
akito0802 <light.of.king.a@gmail.com>
2026-08-03 05:44:37 +0900

    クラウド正式版のキャッシュを更新

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='neet-note-v3';
+const CACHE='neet-note-v4';
 const CORE=['./','./?mode=note','./index.html','./tools.html','./style.css','./background.css','./chord-picker.css','./print.css','./common-ui.css','./app.js','./structure-sync.js','./chord-picker.js','./print.js','./favicon.svg','./apple-touch-icon.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
