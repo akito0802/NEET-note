d9b58bc0430693f7d094cf254435f0df6530be67
akito0802 <light.of.king.a@gmail.com>
2026-08-03 05:20:17 +0900

    Connect NEET NOTE to Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// Firebase ConsoleでWebアプリを追加し、表示された設定値に置き換えてください。
-// このファイルに秘密鍵は含まれませんが、Firestoreのセキュリティルール設定は必須です。
-window.NEET_FIREBASE_CONFIG = window.NEET_FIREBASE_CONFIG || {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+window.NEET_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCc0sOk8DjbS5McxqjIh8ums0FFJfkm9q8",
+  authDomain: "neet-note.firebaseapp.com",
+  projectId: "neet-note",
+  storageBucket: "neet-note.firebasestorage.app",
+  messagingSenderId: "176196540165",
+  appId: "1:176196540165:web:78a3f5af002b4f5445496c",
+  measurementId: "G-F5F2ZD45C9"
 };
