10d33635ef67bd502c5d3a4ac7f746981c5d4c52
akito0802 <light.of.king.a@gmail.com>
2026-08-03 04:54:06 +0900

    オフライン対応のService Workerを追加

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE='neet-note-v1';
+const CORE=['./','./?mode=note','./index.html','./tools.html','./style.css','./background.css','./chord-picker.css','./print.css','./common-ui.css','./common-ui.js','./app.js','./structure-sync.js','./chord-picker.js','./print.js','./favicon.svg','./apple-touch-icon.png','./manifest.webmanifest','./cloud-sync.js','./firebase-config.js'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==location.origin)return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));
+});
