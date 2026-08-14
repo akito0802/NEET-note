f2972cec0350f88cfde8c047f89e1bbc0b2753b2
akito0802 <light.of.king.a@gmail.com>
2026-08-14 13:49:01 +0900

    Force current top route after global menu loads

--- global-menu-v4.js
+++ global-menu-v4.js
@@ -2,8 +2,17 @@
 'use strict';
 if(window.__NEET_MENU_V4_LOADER__)return;
 window.__NEET_MENU_V4_LOADER__=true;
-const s=document.createElement('script');
-s.src='https://akito0802.github.io/NEET-note/global-menu.js?v=20260813-4';
-s.defer=true;
-document.head.appendChild(s);
+const ROOT='https://akito0802.github.io/NEET-note/';
+const menu=document.createElement('script');
+menu.src=ROOT+'global-menu.js?v=20260814-1';
+menu.defer=true;
+menu.onload=()=>{
+  if(document.querySelector('script[data-neet-menu-dedupe]'))return;
+  const dedupe=document.createElement('script');
+  dedupe.src=ROOT+'menu-dedupe.js?v=20260814-1';
+  dedupe.defer=true;
+  dedupe.dataset.neetMenuDedupe='1';
+  document.head.appendChild(dedupe);
+};
+document.head.appendChild(menu);
 })();
