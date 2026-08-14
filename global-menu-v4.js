e5ad4e9b23c69738d19b5c88cf75c4eeaf31b7c2
akito0802 <light.of.king.a@gmail.com>
2026-08-14 18:18:11 +0900

    Load dedicated home routing build

--- global-menu-v4.js
+++ global-menu-v4.js
@@ -4,16 +4,16 @@ if(window.__NEET_MENU_V4_LOADER__)return;
 window.__NEET_MENU_V4_LOADER__=true;
 const ROOT='https://akito0802.github.io/NEET-note/';
 const fix=document.createElement('script');
-fix.src=ROOT+'home-route-fix.js?v=20260814-6';
+fix.src=ROOT+'home-route-fix.js?v=20260814-7';
 fix.defer=true;
 fix.onload=()=>{
   const menu=document.createElement('script');
-  menu.src=ROOT+'global-menu.js?v=20260814-6';
+  menu.src=ROOT+'global-menu.js?v=20260814-7';
   menu.defer=true;
   menu.onload=()=>{
     if(document.querySelector('script[data-neet-menu-dedupe]'))return;
     const dedupe=document.createElement('script');
-    dedupe.src=ROOT+'menu-dedupe.js?v=20260814-6';
+    dedupe.src=ROOT+'menu-dedupe.js?v=20260814-7';
     dedupe.defer=true;
     dedupe.dataset.neetMenuDedupe='1';
     document.head.appendChild(dedupe);
